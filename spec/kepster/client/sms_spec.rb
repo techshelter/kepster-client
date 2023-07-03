99fc04a05ec9487faf58c7102f32e19da0e3ccad
vadjeneka <vadjeneka210@gmail.com>
2023-07-03 16:31:49 +0000

    Pulling main from various fix

--- spec/kepster/client/sms_spec.rb
+++ spec/kepster/client/sms_spec.rb
@@ -5,13 +5,13 @@ RSpec.describe Kepster::Client::SMS do
     let(:subject) { described_class.new.register(**payload)}
     let(:payload) do
       {
-        phone_number:Faker::PhoneNumber.cell_phone,
+        phone_number: Faker::PhoneNumber.phone_number,
         first_name: Faker::Name.first_name,
         last_name: Faker::Name.last_name,
         core_group_id: group_id,
-        fields: {email: Faker::Internet.email},
       }
     end
+
     context "registration is not allowed" do
       let(:group_id) { ENV['KEPSTER_SMS_NO_REGISTER_GROUP_ID'] }
 
@@ -35,54 +35,4 @@ RSpec.describe Kepster::Client::SMS do
       end
     end
   end
-
-  describe 'authenticate' do
-    let(:subject) { described_class.new.authenticate(**payload)}
-    let(:payload) do
-      {
-        email:,
-        group_id:,
-        redirect_url: ENV['KEPSTER_REDIRECT_URL']
-      }
-    end
-
-    context 'when user does not exist' do
-      let(:email) { 'fake@example.com' }
-      let(:group_id) { ENV['KEPSTER_EMAIL_NO_REGISTER_GROUP_ID'] }
-
-      it 'successfully resends the email' do
-        VCR.use_cassette('email/authenticate/email_does_not_exist') do
-          response = subject
-          expect(response["message"]).to eq("Email has been sent")
-          expect(response["success"]).to be_truthy
-        end
-      end
-    end
-
-    context 'when user exists' do
-      let(:email) { Faker::Internet.email }
-      let(:group_id) { ENV['KEPSTER_EMAIL_REGISTER_GROUP_ID'] }
-      before do
-        payload = {
-          first_name: Faker::Name.first_name,
-          last_name: Faker::Name.last_name,
-          email:,
-          core_group_id: group_id,
-          redirect_url: ENV['KEPSTER_REDIRECT_URL']
-        }
-
-        described_class.new.register(**payload)
-      end
-
-      it 'successfully resends the email' do
-        VCR.use_cassette('email/authenticate/email_exists') do
-          response = subject
-          expect(response["message"]).to eq("Email has been sent")
-          expect(response["success"]).to be_truthy
-        end
-      end
-    end
-  end
-
-  # TODO: logout
 end
