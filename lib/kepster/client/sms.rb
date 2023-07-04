176bdb426b2d8c589a6ee1c31353cf9336108c5b
vadjeneka <vadjeneka210@gmail.com>
2023-07-04 16:44:03 +0000

    Fixing sms_spec

--- lib/kepster/client/sms.rb
+++ lib/kepster/client/sms.rb
@@ -4,7 +4,7 @@ module Kepster
   module Client
     class SMS < Base
 
-      def register(first_name:, last_name:, phone_number:, core_group_id:, fields:)
+      def register(first_name:, last_name:, phone_number:, core_group_id:)
         path = '/api/sms/register'
         payload = {
           "register_params" => 
@@ -16,34 +16,22 @@ module Kepster
         response.parsed_response
       end
 
-      # def authenticate(email:, group_id:, redirect_url:)
-      #   path = '/api/email/authenticate'
-      #   payload = { email:, group_id:, redirect_url: }
-      #   token = x_kepster_token(payload, path)
-      #   response = self.class.post(path, headers: headers(token), query: payload)
-      #   response.parsed_response
-      # end
+      def validate_registration_otp(phone_number:, otp_sended:, group_id:)
+        path = '/api/sms/register/token'
+        payload = { phone_number:, otp_sended:, group_id: }
+        token = x_kepster_token(payload, path)
+        response = self.class.get(path, headers: headers(token), query: payload)
+        response.parsed_response
+      end
 
-      # def logout(refresh_token)
-      #   path = '/api/email/logout'
-      #   payload = { refresh_token:}
-      #   token = x_kepster_token(payload, path)
-      #   self.class.delete(path, headers: headers(token), query: payload)
-      # end
+      def authenticate(phone_number:, group_id:)
+        path = '/api/sms/authenticate'
+        payload = { phone_number:, group_id: }
+        token = x_kepster_token(payload, path)
+        response = self.class.post(path, headers: headers(token), query: payload)
+        response.parsed_response
+      end
 
-      # def validate_token(token, user_id)
-      #   path = "/api/email/token"
-      #   payload = {code: token, auth_user_id: user_id}
-      #   token = x_kepster_token(payload, path)
-      #   self.class.get(path, headers: headers(token), query: payload)
-      # end
-
-      # def refresh_token(refresh_token)
-      #   path = "/api/email/token"
-      #   payload = {refresh_token:}
-      #   token = x_kepster_token(payload, path)
-      #   self.class.get(path, headers: headers(token), query: payload)
-      # end
     end
   end
 end
--- spec/kepster/client/sms_spec.rb
+++ spec/kepster/client/sms_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Kepster::Client::SMS do
     let(:subject) { described_class.new.register(**payload)}
     let(:payload) do
       {
-        phone_number: Faker::PhoneNumber.phone_number,
+        phone_number: Faker::PhoneNumber.cell_phone_in_e164,
         first_name: Faker::Name.first_name,
         last_name: Faker::Name.last_name,
         core_group_id: group_id,
@@ -28,11 +28,104 @@ RSpec.describe Kepster::Client::SMS do
       it 'successfully registers the user' do
         VCR.use_cassette('sms/register/registration_allowed') do
           response = subject
-          expect(response).to have_key("core_user_id")
-          expect(response["message"]).to eq("link succesfully sent by e-mail")
-          expect(response["success"]).to be_truthy
+          expect(response["message"]["message"]).to eq("registration created and code succesfully sent by sms")
+          expect(response["message"]["success"]).to be_truthy
         end
       end
     end
+  end
+
+  describe 'authenticate' do
+    let(:subject) { described_class.new.authenticate(**payload)}
+    let(:payload) do
+      {
+        phone_number:,
+        group_id:,
+      }
+    end
+
+    context 'when user does not exist' do
+      let(:phone_number) { '+3853851584495' }
+      let(:group_id) {ENV['KEPSTER_SMS_NO_REGISTER_GROUP_ID'] }
+
+      it 'return user not found' do
+        VCR.use_cassette('sms/authenticate/phone_number_does_not_exist') do
+          response = subject
+          expect(response["message"]["message"]).to eq("Auth user not found")
+        end
+      end
+    end
+
+    # context 'when user exists' do
+    #   let(:phone_number) { Faker::PhoneNumber.cell_phone_in_e164 }
+    #   let(:group_id) { ENV['KEPSTER_SMS_REGISTER_GROUP_ID'] }
+
+    #   before do
+    #     payload = {
+    #       first_name: Faker::Name.first_name,
+    #       last_name: Faker::Name.last_name,
+    #       phone_number: Faker::PhoneNumber.cell_phone_in_e164,
+    #       core_group_id: group_id
+    #     }
+    #     described_class.new.register(**payload)
+    #   end
+
+    #   it 'successfully code sent' do
+    #     VCR.use_cassette('sms/authenticate/phone_number_exists') do
+    #       response = subject
+    #       expect(response["message"]).to eq("code de verification envoyé")
+    #     end
+    #   end
+    # end
+
+  end
+
+  describe 'verify registration code sent' do
+    let(:subject) { described_class.new.validate_registration_otp(**payload) }
+    let(:payload) do
+      {
+        phone_number:,
+        group_id:,
+        otp_sended:
+      }
+    end
+
+    context 'when phone_number is not valid' do
+      let(:phone_number) { Faker::PhoneNumber.cell_phone_in_e164 }
+      let(:group_id) { ENV['KEPSTER_SMS_REGISTER_GROUP_ID'] }
+      let(:otp_sended) { '123456'}
+
+      it 'return failure' do
+        VCR.use_cassette('sms/verification/invalid_phone_number') do
+          response = subject
+          expect(response["message"]).to eq("Failure : verification fail, Registration not found")
+        end
+      end
+
+    end
+
+    # context 'when otp code is not valid' do
+    #   let(:phone_number) { Faker::PhoneNumber.cell_phone_in_e164 }
+    #   let(:group_id) { ENV['KEPSTER_SMS_REGISTER_GROUP_ID'] }
+    #   let(:otp_sended) { '123456'}
+
+    #   before do
+    #     payload = {
+    #       first_name: Faker::Name.first_name,
+    #       last_name: Faker::Name.last_name,
+    #       phone_number: Faker::PhoneNumber.cell_phone_in_e164,
+    #       core_group_id: group_id
+    #     }
+    #     described_class.new.register(**payload)
+    #   end
+
+    #   it 'bad otp' do
+    #     VCR.use_cassette('sms/veriferication/invalid_otp') do
+    #       response = subject
+    #       expect(response["message"]).to eq("Failure : verification fail, Otp not valid")
+    #     end
+    #   end
+
+    # end
   end
 end
